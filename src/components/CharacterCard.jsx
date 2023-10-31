5423c10e6a28ad3c9ff679bed4cd522388bc433d
Sol Cifuentes <msolcifuentes@gmail.com>
2023-10-31 01:38:57 +0100

    html card refactor

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -5,23 +5,21 @@ import styles from "./CharacterCard.module.css";
 export default function CharacterCard(props) {
   const character = props.characterData;
   return (
-    <div>
-      <ul>
-        {character && (
-          <li className={styles.card}>
-            <img
-              className={styles.characterImg}
-              src={character.image}
-              alt={character.name}
-            />
-            <div className={styles.cardContainer}>
-              <h3 className={styles.characterName}>{character.name}</h3>
-              <p className={styles.characterSpecies}>{character.species}</p>
-              <p className={styles.characterOrigin}>{character.origin.name}</p>
-            </div>
-          </li>
-        )}
-      </ul>
-    </div>
+    <li className={styles.card}>
+      {character && (
+        <div>
+          <img
+            className={styles.characterImg}
+            src={character.image}
+            alt={character.name}
+          />
+          <div className={styles.cardContainer}>
+            <h3 className={styles.characterName}>{character.name}</h3>
+            <p className={styles.characterSpecies}>{character.species}</p>
+            <p className={styles.characterOrigin}>{character.origin.name}</p>
+          </div>
+        </div>
+      )}
+    </li>
   );
 }

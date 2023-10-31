affad1241df1ec74365679811edbfee30ac8252f
Sol Cifuentes <msolcifuentes@gmail.com>
2023-10-31 16:21:02 +0100

    conditional styling circle

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./CharacterCard.module.css";
 
 export default function CharacterCard(props) {
   const character = props.characterData;
+
   return (
     <li className={styles.card}>
       <img
@@ -14,8 +15,17 @@ export default function CharacterCard(props) {
       <div className={styles.cardContainer}>
         <h3 className={styles.characterName}>{character.name}</h3>
         <div className={styles.speciesContainer}>
-          <span className={styles.circle}></span>
-
+          {/* Lo atamos con alambre. styling provisional */}
+          {character.status === "Alive" && (
+            <span className={styles.circleAlive}></span>
+          )}
+          {character.status === "Dead" && (
+            <span className={styles.circleDead}></span>
+          )}
+          {character.status === "unknown" && (
+            <span className={styles.circleUnknown}></span>
+          )}
+          {/* acá finaliza */}
           <p className={styles.characterSpecies}>{character.species}</p>
         </div>
         <p className={styles.characterOrigin}>{character.origin.name}</p>
@@ -23,3 +33,9 @@ export default function CharacterCard(props) {
     </li>
   );
 }
+/*
+Create a condition
+"Alive" : green; 
+"Dead": red; 
+"unknown / the rest": yellow; 
+*/
